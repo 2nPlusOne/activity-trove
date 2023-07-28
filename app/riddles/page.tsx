c280408e1a075515e2eb2a64991a6d15ddac564f
2nPlusOne <39078738+2nPlusOne@users.noreply.github.com>
2023-07-27 21:50:54 -0400

    added riddles page

--- app/riddles/page.tsx
+++ app/riddles/page.tsx
@@ -1,12 +1,19 @@
 import { Metadata } from "next/types"
+import Riddle from "./riddle"
 
 export const metadata: Metadata = {
   title: 'Riddles',
   description: 'For minds that love a twisty tour, the sphinx awaits to test your lore.',
 }
 
-export default function Jokes() {
+export default function Riddles() {
   return (
-    ''
+    <>
+      <div className="text-center flex flex-col items-center w-full max-w-xl">
+        <h1 className="text-3xl text-emerald-400">Riddles</h1>
+        <p className="italic text-gray-400">For minds that love a twisty tour, the sphinx awaits to test your lore.</p>
+        <Riddle />
+      </div>
+    </>
   )
 }
