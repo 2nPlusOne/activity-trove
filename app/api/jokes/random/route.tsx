ab4ec8c8d0b8408c21600d7f27f73f323b537905
2nPlusOne <39078738+2nPlusOne@users.noreply.github.com>
2023-07-28 01:27:50 -0400

    added jokes route

---
+++ app/api/jokes/random/route.tsx
@@ -0,0 +1,24 @@
+// this file is a nextjs 13 app router api route handler for /api/jokes
+
+import jokes from "../jokes.json";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.nextUrl);
+  let data = {};
+
+  // if the request has a search param of safe-mode,
+  // then only use the jokes saved in the jokes.json file
+  const useExternal = !searchParams.has("strict-mode") && Math.random() > 0.5;
+  if (useExternal) {
+    data = await fetch("https://official-joke-api.appspot.com/random_joke").then(
+      (res) => res.json()
+    );
+  } else {
+    data = jokes[Math.floor(Math.random() * jokes.length)];
+    await new Promise((resolve) => setTimeout(resolve, 500));
+  }
+
+  // return the joke as json
+  return NextResponse.json(data);
+}
