c96d87c36fce94910d07835d1d83de9455dff925
2nPlusOne <39078738+2nPlusOne@users.noreply.github.com>
2023-07-29 23:41:13 -0400

    Build trivia router page

--- app/trivia/page.tsx
+++ app/trivia/page.tsx
@@ -1,12 +1,19 @@
 import { Metadata } from "next/types"
+import Trivia from "./trivia"
 
 export const metadata: Metadata = {
   title: 'Trivia',
   description: 'Quizzes and facts, many an option, ready to test your trivia noggin?',
 }
 
-export default function Trivia() {
+export default function Jokes() {
   return (
-    ''
+    <>
+      <div className="text-center flex flex-col items-center w-full max-w-xl">
+        <h1 className="text-3xl text-emerald-400">{metadata.title as string}</h1>
+        <p className="italic text-gray-400">{metadata.description}</p>
+        <Trivia />
+      </div>
+    </>
   )
 }
