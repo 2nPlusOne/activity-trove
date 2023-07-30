02730c7b006d2f3a62d79b793d9753759bfa42bf
2nPlusOne <39078738+2nPlusOne@users.noreply.github.com>
2023-07-29 23:39:22 -0400

    Started trivia.tsx and Finished triviaConfigForm

---
+++ app/trivia/trivia.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import React from "react";
+import TriviaConfigForm from "./triviaConfigForm";
+import { TriviaConfigState, defaultTriviaConfig } from "../../types/triviaTypes";
+import { Question } from "../../types/quizTypes";
+import Quiz from "./_quiz/quiz";
+import { TriviaApiResponse } from "@/types/openTdbTypes";
+
+export default function Trivia() {
+  const fetchBaseUrl = "/api/trivia/";
+  const [triviaConfig, setTriviaConfig] = React.useState<TriviaConfigState>(defaultTriviaConfig);
+  const [questions, setQuestions] = React.useState<Question[] | null>(null);
+  const [loading, setLoading] = React.useState<boolean>(false);
+
+  const onGenerateTrivia = async (e: React.FormEvent<HTMLButtonElement>) => {
+    setLoading(true);
+    e.preventDefault();
+    setTriviaConfig(triviaConfig);
+    // configure the fetch url based on the triviaConfig
+    const fetchUrl = `${fetchBaseUrl}?amount=${triviaConfig.numQuestions}${triviaConfig.category ? `&category=${triviaConfig.category}` : ""}${triviaConfig.difficulty ? `&difficulty=${triviaConfig.difficulty}` : ""}${triviaConfig.type != "" ? `&type=${triviaConfig.type }` : ""}`;
+    // fetch the trivia data
+    const response: TriviaApiResponse = await fetch(fetchUrl).then((res) => res.json());
+    const questions = response.questions;
+    // set the quiz data
+    setQuestions(questions);
+    setLoading(false);
+  };
+
+  const onFinish = () => {
+    setQuestions(null);
+  };
+  
+  return (
+    <>
+      <div className="mt-8 flex flex-col justify-center items-center gap-y-2 p-4 w-full h-auto min-h-[435px] bg-slate-900 rounded-lg">
+        {
+          loading ? (
+            <span className="loading loading-spinner text-emerald-400 w-1/5" />
+          )
+          : questions ? (
+            <Quiz questions={questions} onFinish={onFinish} />
+          )
+          : (
+            <TriviaConfigForm triviaConfig={triviaConfig} setTriviaConfig={setTriviaConfig} generateTrivia={onGenerateTrivia} />
+          )
+        }
+      </div>
+    </>
+  );
+}
---
+++ app/trivia/triviaConfigForm.tsx
@@ -0,0 +1,116 @@
+"use client";
+
+import React from "react";
+import {
+  TriviaConfigFormProps,
+  triviaCategories,
+  triviaTypesMap,
+  TriviaTypeEnum,
+  TriviaDifficultyEnum,
+  triviaDifficultiesMap,
+  TriviaCategoryEnum,
+} from "../../types/triviaTypes";
+
+const TriviaConfigForm: React.FC<TriviaConfigFormProps> = ({
+  triviaConfig,
+  setTriviaConfig,
+  generateTrivia,
+}) => {
+  return (
+    <>
+      <h2 className="text-xl text-emerald-400">Configure a Question Set</h2>
+      <form className="flex flex-col justify-center items-center w-full h-full gap-y-2">
+        {/* a form with a select for the number of questions, category, difficulty, and type */}
+        <div className="flex flex-col justify-center items-center w-full h-auto">
+          <label htmlFor="numQuestions">Number of Questions</label>
+          <input
+            id="numQuestions"
+            name="numQuestions"
+            type="number"
+            min={1}
+            max={10}
+            className="bg-slate-100 text-slate-700 rounded-md focus:border-emerald-400 w-2/3 mx-8"
+            value={triviaConfig.numQuestions}
+            onChange={(e) =>
+              setTriviaConfig({
+                ...triviaConfig,
+                numQuestions: parseInt(e.target.value),
+              })
+            }
+          />
+        </div>
+        <div className="flex flex-col justify-center items-center w-full h-auto">
+          <label htmlFor="category">Category</label>
+          <select
+            id="category"
+            name="category"
+            className="bg-slate-100 text-slate-700 rounded-md focus:border-emerald-400 w-2/3 mx-8"
+            value={triviaConfig.category}
+            onChange={(e) =>
+              setTriviaConfig({
+                ...triviaConfig,
+                category: parseInt(e.target.value) as TriviaCategoryEnum,
+              })
+            }
+          >
+            {Array.from(triviaCategories, ([value, text]) => (
+              <option key={value} value={value}>
+                {text}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="flex flex-col justify-center items-center w-full h-auto">
+          <label htmlFor="type">Difficulty</label>
+          <select
+            id="difficulty"
+            name="difficulty"
+            className="bg-slate-100 text-slate-700 rounded-md focus:border-emerald-400 w-2/3 mx-8"
+            value={triviaConfig.difficulty}
+            onChange={(e) =>
+              setTriviaConfig({
+                ...triviaConfig,
+                difficulty: e.target.value as TriviaDifficultyEnum,
+              })
+            }
+          >
+            {Array.from(triviaDifficultiesMap, ([value, text]) => (
+              <option key={value} value={value}>
+                {text}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="flex flex-col justify-center items-center w-full h-auto">
+          <label htmlFor="type">Type</label>
+          <select
+            id="type"
+            name="type"
+            className="bg-slate-100 text-slate-700 rounded-md focus:border-emerald-400 w-2/3 mx-8"
+            value={triviaConfig.type}
+            onChange={(e) =>
+              setTriviaConfig({
+                ...triviaConfig,
+                type: e.target.value as TriviaTypeEnum,
+              })
+            }
+          >
+            {Array.from(triviaTypesMap, ([value, text]) => (
+              <option key={value} value={value}>
+                {text}
+              </option>
+            ))}
+          </select>
+        </div>
+        <button
+          className="font-semibold p-4 mt-4 border-2 border-emerald-400 text-emerald-400 rounded-md bg-slate-800 hover:bg-emerald-400 hover:text-slate-800 transition-all"
+          onClick={generateTrivia}
+        >
+          Generate Trivia Questions!
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default TriviaConfigForm;
