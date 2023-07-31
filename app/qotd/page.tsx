54af6660a65605e0a9b96d2649827e7ba711ea6a
2nPlusOne <39078738+2nPlusOne@users.noreply.github.com>
2023-07-31 00:55:59 -0400

    Added placeholder question of the day page

---
+++ app/qotd/page.tsx
@@ -0,0 +1,19 @@
+import { Metadata } from "next/types"
+import Riddle from "./riddle"
+
+export const metadata: Metadata = {
+  title: 'Question of the Day',
+  description: 'For minds that love a twisty tour, the sphinx awaits to test your lore.',
+}
+
+export default function Riddles() {
+  return (
+    <>
+      <div className="text-center flex flex-col items-center w-full max-w-xl">
+        <h1 className="text-3xl text-emerald-400">{metadata.title as string}</h1>
+        <p className="italic text-gray-400">{metadata.description}</p>
+        <Riddle />
+      </div>
+    </>
+  )
+}
