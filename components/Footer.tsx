63b6b1acd112046e4c06f9fe6646192cce7bc4b1
2nPlusOne <39078738+2nPlusOne@users.noreply.github.com>
2023-08-06 03:01:47 -0400

    Added GitHub repo to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,38 +1,9 @@
 import React from "react";
 
-interface FooterNavItemProps {
-  title: string;
-  route: string;
-  info: string;
-}
-
-const footerNavs: FooterNavItemProps[] = [
-  {
-    title: "/jokes",
-    route: "/jokes",
-    info: "Puns and punchlines, gags and groans, we've got jokes to lighten the tone.",
-  },
-  {
-    title: "/riddles",
-    route: "#riddles",
-    info: "For minds that love a twisty tour, the sphinx awaits to test your lore.",
-  },
-  {
-    title: "/trivia",
-    route: "#trivia",
-    info: "Quizzes and facts, many an option, ready to test your trivia noggin?",
-  },
-  {
-    title: "/resources",
-    route: "#resources",
-    info: "Plenty of choices to be found, here you will find resources abound.",
-  },
-];
-
 const Footer: React.FC = () => {
   return (
     <footer className="flex items-center justify-between w-full h-auto p-3">
-      <div className="w-12 h-12" /> {/* Empty div to balance left side */}
+      <GitHub />
       <div className="flex items-center justify-center">
         Made with&nbsp;
         <div className="hover:animate-heartBeat px-[1px]">  
@@ -53,12 +24,29 @@ const Footer: React.FC = () => {
         </a>
         &nbsp;&nbsp;
       </div>
-      <div className="flex justify-end">
-        <BuyMeACoffee />
-      </div>
+      <BuyMeACoffee />
     </footer>
   );
 };
+
+const GitHub: React.FC = () => {
+  return (
+    <a
+      title="GitHub"
+      href="https://github.com/2nPlusOne/activity-center"
+      target="_blank"
+      rel="noreferrer"
+      className="group flex flex-col items-center justify-center w-12 h-10 rounded-full
+        hover:scale-110 focus:scale-110 transition-all duration-200 py-0"
+    >
+      <div className="hover:animate-wiggle group-focus:animate-wiggle">
+        <svg className="w-6 h-6 fill-slate-400" aria-hidden="true" viewBox="0 0 16 16" data-view-component="true">
+          <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
+        </svg>
+      </div>
+    </a>
+  );
+}
 
 const BuyMeACoffee: React.FC = () => {
   return (
@@ -79,26 +67,6 @@ const BuyMeACoffee: React.FC = () => {
             <line x1="14" y1="1" x2="14" y2="4" />
           </svg>
         </div>
-    </a>
-  );
-};
-
-const FooterNavItem: React.FC<FooterNavItemProps> = (
-  props: FooterNavItemProps
-) => {
-  const { title, route, info } = props;
-  return (
-    <a
-      href={route}
-      className="group rounded-lg border border-transparent p-2 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-    >
-      <h2 className={`mb-3 text-xl font-semibold`}>
-        {title}{" "}
-        <span className="inline-block transition-transform group-hover:translate-x-2 group-focus:translate-x-2 motion-reduce:transform-none">
-          -&gt;
-        </span>
-      </h2>
-      <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>{info}</p>
     </a>
   );
 };
